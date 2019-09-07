149596b7615f4566e064c1d5f5dbd3c0940ae4b4
eonil <pub.dev@eonil.com>
2019-09-07 20:45:32 +0700

    Erase bad comment.

--- Sources/CodeView5/CodePosition.swift
+++ Sources/CodeView5/CodePosition.swift
@@ -8,25 +8,11 @@
 
 import Foundation
 
-/// Selection to be used for external communication.
-struct CodeSelection {
-    var range = CodePosition.zero..<CodePosition.zero
-}
 struct CodePosition: Comparable {
     /// Offset from start in line list in storage.
     var line = 0
     /// Offset from start in characters in target line.
     /// This is number of `Character`s from start of the line.
-    /// 
-    /// In most cases, code line do not exceed 128 characters
-    /// counting them for each time would be easier to maintain.
-    ///
-    /// Having extremly long line is strongly discouraged
-    /// and won't work well with this view.
-    ///
-    /// `CodeView` also keeps whole pre-resolved indices
-    /// for each columns for current line. Mapping would be
-    /// trivial.
     var column = 0
     static let zero = CodePosition(line: 0, column: 0)
     static func < (_ a:CodePosition, _ b:CodePosition) -> Bool {
